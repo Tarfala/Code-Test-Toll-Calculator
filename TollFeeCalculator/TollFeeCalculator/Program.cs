e36146737704c3218e5338c59f0a81561c44753b
Tarfala <43854579+Tarfala@users.noreply.github.com>
2022-06-19 11:44:13 +0200

    Added base code to a console app project.

---
+++ TollFeeCalculator/TollFeeCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TollFeeCalculator
+{
+    internal class Program
+    {    
+        // TODO JR: Remove Main and Program.cs, only used for testing purpose before tests are added
+        static void Main(string[] args)
+        {
+            DateTime[] dates = new DateTime[] {
+                new DateTime(2022, 06, 17, 08, 00, 00),
+                new DateTime(2022, 06, 18, 08, 00, 00)         
+            };
+            Car vehicle = new Car();
+            int result = new TollCalculator().GetTollFee(vehicle, dates);
+            Console.ReadLine();
+        }
+    }
+}
